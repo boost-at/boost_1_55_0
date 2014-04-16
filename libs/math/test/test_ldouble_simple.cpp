f979dea5d85fbb0e9d93050d3b2c00a96d22063f
Adhemerval Zanella <azanella@linux.vnet.ibm.com>
2014-04-15 21:11:34 -0500

    math: Fix test_ldouble_simple build failure

--- libs/math/test/test_ldouble_simple.cpp
+++ libs/math/test/test_ldouble_simple.cpp
@@ -13,13 +13,13 @@
 BOOST_AUTO_TEST_CASE( test_main )
 {
    BOOST_CHECK_EQUAL((boost::math::signbit)(1.0L), 0.0L);
-   BOOST_CHECK_EQUAL((boost::math::signbit)(-1.0L), 1.0L);
+   BOOST_CHECK((boost::math::signbit)(-1.0L) != 0);
    BOOST_CHECK_EQUAL((boost::math::sign)(1.0L), 1.0L);
    BOOST_CHECK_EQUAL((boost::math::sign)(-1.0L), -1.0L);
    BOOST_CHECK_EQUAL((boost::math::changesign)(1.0L), -1.0L);
    BOOST_CHECK_EQUAL((boost::math::changesign)(-1.0L), 1.0L);
 
-   BOOST_CHECK_EQUAL((boost::math::fpclassify)(1.0L), FP_NORMAL);
+   BOOST_CHECK_EQUAL((boost::math::fpclassify)(1.0L), (int)FP_NORMAL);
    BOOST_CHECK_EQUAL((boost::math::isnan)(1.0L), false);
    BOOST_CHECK_EQUAL((boost::math::isinf)(1.0L), false);
    BOOST_CHECK_EQUAL((boost::math::isnormal)(1.0L), true);
